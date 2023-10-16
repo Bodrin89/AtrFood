e1fbc1730a5dcebb485b5b956a0f15395fd928ed
Мурад Камылов <kamylov@bk.ru>
2023-10-16 15:46:25 +0400

    add validation

--- apps/user/serializers.py
+++ apps/user/serializers.py
@@ -1,7 +1,10 @@
+from django.contrib.auth import authenticate, get_user_model
 from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 
 from apps.user.models import AddressModel, BaseUserModel, RegionModel
+
+User = get_user_model()
 
 
 class LoginSerializer(serializers.ModelSerializer):
@@ -12,21 +15,46 @@ class LoginSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = BaseUserModel
-        fields = ('id', 'email', 'password')
+        fields = ('id', 'email', 'password',)
         read_only_fields = ('id',)
+
+    def validate(self, data):
+        email = data.get('email')
+        user = User.objects.filter(email=email).first()
+        if user is not None and not user.is_active:
+            raise serializers.ValidationError('Аккаунт необходимо подтвердить по электронной почте.')
+
+        return data
 
 
 class RegionSerializer(serializers.ModelSerializer):
+    """Сериализатор региона"""
+
 
     class Meta:
         model = RegionModel
         fields = ('region', 'city')
-        read_only_fields = ('id',)
+        # read_only_fields = ('id',)
 
 
 class AddressSerializer(serializers.ModelSerializer):
+    """Сериализатор для создания адреса"""
 
     class Meta:
         model = AddressModel
-        fields = ('district', 'street', 'house_number', 'apartment_number', 'floor')
+        fields = ('id', 'district', 'street', 'house_number', 'apartment_number', 'floor')
         read_only_fields = ('id',)
+
+    def validate(self, attrs):
+        request = self.context.get('request')
+        user = request.user
+        if user.is_authenticated and not self.instance:
+            if AddressModel.objects.filter(user=user).count() >= 3 and request.method != 'PUT':
+                raise serializers.ValidationError("Вы не можете добавить более трех адресов.")
+        return attrs
+
+    def create(self, validated_data):
+        request = self.context.get('request')
+        validated_data['user'] = request.user
+        return super().create(validated_data)
+
