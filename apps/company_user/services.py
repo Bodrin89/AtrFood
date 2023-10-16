b15283902f7b30073ff3788e9a5a9de1e75f0328
Мурад Камылов <kamylov@bk.ru>
2023-10-16 15:46:48 +0400

    change CompanyUserServices

--- apps/company_user/services.py
+++ apps/company_user/services.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import login
 from django.contrib.auth.hashers import make_password
 from django.db import transaction
 
@@ -9,7 +10,7 @@ from config.settings import LOGGER
 class CompanyUserServices:
 
     @staticmethod
-    def create_company(validated_data: dict) -> CompanyUserModel:
+    def create_company(request, validated_data: dict) -> CompanyUserModel:
         """Создание нового пользователя и сохранение его в БД с захэшированным паролем"""
         with transaction.atomic():
             del validated_data['password_repeat']
@@ -19,12 +20,18 @@ class CompanyUserServices:
             bank_ = bank.replace(" ", "")
             contact_person_data = validated_data.pop('contact_person', None)
             company_address_data = validated_data.pop('company_address', None)
-            address_data = validated_data.pop('address', None)
+            addresses_data = validated_data.pop('addresses', [])
             contact_person = ContactPersonModel.objects.create(**contact_person_data)
             region_data = validated_data.pop('region', None)
             region, created = RegionModel.objects.get_or_create(**region_data)
             company_address, created = CompanyAddress.objects.get_or_create(**company_address_data)
             company_user = CompanyUserModel.objects.create(region=region, bank=bank_, company_address=company_address,
-                                                           contact_person=contact_person, **validated_data)
-            AddressModel.objects.create(user=company_user, **address_data)
+                                                           contact_person=contact_person,
+                                                           #is_active=False,
+                                                           user_type='company',
+                                                           **validated_data)
+            # AddressModel.objects.create(user=company_user, **address_data)
+            for address_data in addresses_data:
+                AddressModel.objects.create(user=company_user, **address_data)
+            login(request, company_user)
         return company_user
