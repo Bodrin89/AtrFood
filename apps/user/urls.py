3a2f81cfc947c3f94ad933d786d18d387ee6c4db
Мурад Камылов <kamylov@bk.ru>
2023-10-16 15:49:12 +0400

    register new viewsets

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,8 +1,21 @@
 from django.urls import path
 
-from apps.user.views import LoginView, LogoutView
+from apps.user.views import (
+    LoginView,
+    LogoutView,
+    ProfileViewSet,
+    AddressViewSet,
+    ClientViewSet,
+)
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+
+router.register(r'profile', ClientViewSet, basename='profile')
+router.register(r'full_profile_info', ProfileViewSet, basename='full_profile_info')
+router.register(r'address', AddressViewSet, basename='address')
 
 urlpatterns = [
     path('login', LoginView.as_view(), name='login'),
     path('logout', LogoutView.as_view(), name='logout'),
-]
+] + router.urls
