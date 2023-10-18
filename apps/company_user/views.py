90f5d8c8f57844fe7ccd44fdeb40fd6a45d4403f
Мурад Камылов <kamylov@bk.ru>
2023-10-18 14:49:08 +0400

    email verification views

--- apps/company_user/views.py
+++ apps/company_user/views.py
@@ -1,9 +1,23 @@
+from rest_framework import status
 from rest_framework.generics import CreateAPIView
 
 from apps.company_user.serializers import CreateCompanySerializer
 from apps.user.models import AddressModel
+from rest_framework.response import Response
 
 
 class SingUpCompanyView(CreateAPIView):
     """Регистрация нового юридического пользователя"""
     serializer_class = CreateCompanySerializer
+
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        if response.status_code == status.HTTP_201_CREATED:
+            return Response(
+                {
+                    'status': 'Success',
+                    'message': 'Подтверждение отправлено на ваш электронный адрес.'
+                },
+                status=status.HTTP_201_CREATED
+            )
+        return response
--- apps/user/views.py
+++ apps/user/views.py
@@ -1,11 +1,11 @@
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, get_user_model
 from django.db.models import QuerySet
 from rest_framework import permissions, status, mixins
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, UpdateAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from apps.user.serializers import LoginSerializer, AddressSerializer, RegionSerializer
+from apps.user.serializers import LoginSerializer, AddressSerializer, RegionSerializer, EmailSerializer, ChangePasswordSerializer
 from apps.user.models import AddressModel
 from apps.user.services import UserServices
 from apps.individual_user.models import IndividualUserModel
@@ -14,9 +14,12 @@ from apps.company_user.serializers import CreateCompanySerializer, GetUpdateComp
 from apps.individual_user.serializers import CreateIndividualSerializer, GetUpdateIndividualSerializer
 from rest_framework.viewsets import ReadOnlyModelViewSet, ModelViewSet, GenericViewSet
 
+User = get_user_model()
+
 
 class LoginView(CreateAPIView):
     """Вход в учетную запись"""
+
     serializer_class = LoginSerializer
     permission_classes = [~ permissions.IsAuthenticated]
 
@@ -38,6 +41,7 @@ class LogoutView(APIView):
 
 class ProfileViewSet(ReadOnlyModelViewSet):
     """Просмотр профиля пользователя"""
+
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
@@ -55,6 +59,7 @@ class ProfileViewSet(ReadOnlyModelViewSet):
 
 class AddressViewSet(ModelViewSet):
     """Просмотр и создание нового адреса пользователя отдельно"""
+
     serializer_class = AddressSerializer
     permission_classes = [permissions.IsAuthenticated]
 
@@ -66,6 +71,7 @@ class ClientViewSet(mixins.RetrieveModelMixin,
                     mixins.UpdateModelMixin,
                     GenericViewSet):
     """Просмотр профиля и изменение данных пользователя"""
+
     permission_classes = [permissions.IsAuthenticated, ]
 
     def get_queryset(self):
@@ -80,3 +86,203 @@ class ClientViewSet(mixins.RetrieveModelMixin,
             return GetUpdateCompanySerializer
         return GetUpdateIndividualSerializer
 
+
+class ConfirmEmailView(APIView):
+    """Подтверждение электронной почты при создании нового пользователя"""
+    def get(self, request, *args, **kwargs):
+        token = request.query_params.get('token')
+        user = get_user_model().objects.filter(confirmation_token=token).first()
+        if user:
+            user.is_active = True
+            user.save()
+            return Response(
+                {
+                'status': 'Success',
+                'message': 'Email успешно подтвержден.'
+                },
+                status=status.HTTP_200_OK
+            )
+        return Response(
+            {
+                'status': 'Error',
+                'message': 'Пользователь не найден.'
+            },
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+class EmailUrlView(APIView):
+    """Отправка подтверждения для смены электронной почты"""
+
+    permission_classes = [permissions.IsAuthenticated, ]
+    serializer_class = EmailSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            email = serializer.validated_data['email']
+            if email == request.user.email:
+                return Response(
+                    {
+                        'status': 'Error',
+                        'message': f'У вас уже указана данная почта.'
+                    },
+                    status=status.HTTP_200_OK
+                )
+            token = self.request.user.confirmation_token
+            message = 'Для подтверждения email, пожалуйста, перейдите по ссылке:'
+            subject = 'Подтверждение email'
+            email_url = 'update-email'
+            UserServices.update_email(
+                user_token=token,
+                message=message,
+                subject=subject,
+                email_url=email_url,
+                new_email=email
+            )
+            return Response(
+                {
+                    'status': 'Success',
+                    'message': f'Уведомление отправлено на новую электронную почту: {email}'
+                },
+                status=status.HTTP_200_OK
+            )
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UpdateEmailView(APIView):
+    """Смена Электронной почты"""
+    def get(self, request):
+        token = request.query_params.get('token')
+        email = request.query_params.get('email')
+        if not token or not email:
+            return Response(
+                {
+                    'status': 'Error',
+                    'message': 'Токен и email обязательны.'
+                },
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        user = get_user_model().objects.filter(confirmation_token=token).first()
+        if user:
+            user.email = email
+            user.save()
+            return Response(
+                {
+                    'status': 'Success',
+                    'message': 'Email успешно изменен.'
+                },
+                status=status.HTTP_200_OK
+            )
+        return Response(
+            {
+                'status': 'Error',
+                'message': 'Неверный токен или email.'
+            },
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+class ForgotPasswordView(APIView):
+    """Отправка подтверждения для восстановления пароля"""
+
+    serializer_class = EmailSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            email = serializer.validated_data['email']
+            user = User.objects.filter(email=email).first()
+            if user:
+                token = user.confirmation_token
+                message = 'Для восстановления пароля перейдите по ссылке: '
+                subject = 'Восстановление пароля'
+                email_url = 'update-password'
+                UserServices.confirmation_email(
+                    user_token=token,
+                    message=message,
+                    subject=subject,
+                    email_url=email_url,
+                    user_email=email
+                )
+                return Response(
+                    {
+                        'status': 'Success',
+                        'message': f'Уведомление отправлено на электронную почту. : {email}'
+                    },
+                    status=status.HTTP_200_OK
+                )
+
+        return Response(
+            {
+                'status': 'Error',
+                'message': f'Пользователь с данной электронной почтой не найден'
+            },
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+class UpdatePasswordViewNotInProfile(APIView):
+    """Воссстановление пароля"""
+
+    serializer_class = ChangePasswordSerializer
+
+    def post(self, request, *args, **kwargs):
+        token = request.query_params.get('token')
+        if not token:
+            return Response(
+                {
+                    'status': 'Error',
+                    'message': f'Неверная ссылка.'
+                },
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        user = User.objects.filter(confirmation_token=token).first()
+        if not user:
+            return Response(
+                {
+                    'status': 'Error',
+                    'message': f'Пользователь не найден.'
+                },
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            serializer.update(user, serializer.validated_data)
+            return Response(
+                {
+                    'status': 'Success',
+                    'message': f'Пароль изменен.'
+                },
+                status=status.HTTP_200_OK
+            )
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UpdatePasswordViewInProfile(APIView):
+    """Смена пароля """
+
+    serializer_class = ChangePasswordSerializer
+    permission_classes = [permissions.IsAuthenticated, ]
+
+    def post(self, request, *args, **kwargs):
+        user = request.user
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            serializer.update(user, serializer.validated_data)
+            login(request, user)
+            return Response(
+                {
+                    'status': 'Success',
+                    'message': f'Пароль изменен.'
+                },
+                status=status.HTTP_200_OK
+            )
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
