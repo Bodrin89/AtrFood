fe8838e5eca05d7d715432feb2dc7c9dc147cead
Мурад Камылов <kamylov@bk.ru>
2023-10-24 12:17:49 +0400

    изменен emails, т к уже передается список

--- apps/promotion/tasks.py
+++ apps/promotion/tasks.py
@@ -1,5 +1,4 @@
 import os
-
 from django.core.mail import send_mail
 from dotenv import load_dotenv
 
@@ -9,22 +8,24 @@ load_dotenv()
 from config.celery import app
 
 promotion = 'Новое сообщение'
-emails = os.getenv('TEST_EMAIL')
+# emails = os.getenv('TEST_EMAIL')
 
 # @app.task
 # def send_email_promotion():
 #     send_email_new_promotion(promotion, emails)
 #     LOGGER.debug('Сообщение отправлено')
 
+
 @app.task
-def send_email_promotion(promotion_name):
+def send_email_promotion(promotion_name, emails):
     send_email_new_promotion(promotion_name, emails)
-    LOGGER.debug('Сообщение отправлено')
+    LOGGER.debug(f'Сообщения отправлены')
+
 
 def send_email_new_promotion(promotion, emails):
     send_mail(subject='Новая акция',
               message=promotion,
               from_email=os.getenv('SMTP_USER'),
-              recipient_list=[emails],
+              recipient_list=emails,
               fail_silently=False
               )
